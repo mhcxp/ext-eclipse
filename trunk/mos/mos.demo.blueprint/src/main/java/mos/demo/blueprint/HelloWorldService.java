6d6aac32a6d9c479900c381ca0da4377b85f7a9b
archer.dzh@gmail.com <archer.dzh@gmail.com@4a0e29b9-bc3b-a73f-f274-564cffca560c>
2012-09-20 07:18:00 +0000

    mos开发迁移至ext-eclipse

---
+++ trunk/mos/mos.demo.blueprint/src/main/java/mos/demo/blueprint/HelloWorldService.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2012 Ext-Eclipse Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package mos.demo.blueprint;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import javax.servlet.ServletException;
+
+import org.osgi.service.http.HttpContext;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author dzh
+ * @version 1.0 Sep 14, 2012 4:00:27 PM
+ */
+public class HelloWorldService {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(HelloWorldService.class);
+
+	private HttpService httpService;
+
+	public void setHttpService(HttpService httpService) {
+		this.httpService = httpService;
+	}
+
+	public HttpService getHttpService() {
+		return this.httpService;
+	}
+
+	public void start() throws ServletException, NamespaceException {
+		// create a default context to share between registrations
+		final HttpContext httpContext = httpService.createDefaultHttpContext();
+		// register the hello world servlet
+		final Dictionary<String, String> initParams = new Hashtable<String, String>();
+		initParams.put("from", "HttpService");
+		httpService.registerServlet("/hw", // alias
+				new HelloWorldServlet("/hw"), // registered servlet
+				initParams, // init params
+				httpContext // http context
+				);
+		// register images as resources
+		httpService.registerResources("/images", "/images", httpContext);
+		LOG.info("HelloWorldService started");
+	}
+
+	public void stop() {
+		try {
+			httpService.unregister("/helloworld/hs");
+			httpService.unregister("/*");
+			httpService.unregister("/images");
+		} catch (Exception e) {
+
+		}
+	}
+}
---
+++ trunk/mos/mos.demo.bundle/src/main/java/mos/demo/bundle/Activator.java
@@ -0,0 +1,16 @@
+package mos.demo.bundle;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public void start(BundleContext arg0) throws Exception {
+
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+
+	}
+
+}
