0a923abf112c695e743866e23779d81b74996ef2
anrainie <anrainie@gmail.com>
2012-10-14 08:48:27 +0000

    hibernate

--- mos/mos.hibernate.demo/src/main/java/mos/hibernate/demo/Cat.java
+++ mos/mos.hibernate.demo/src/main/java/mos/hibernate/demo/Cat.java
@@ -1,12 +1,40 @@
 package mos.hibernate.demo;
 
 /**
- *@author caiyu
- *@date 2012-10-11 обнГ2:41:21
+ * @author caiyu
+ * @date 2012-10-11 обнГ2:41:21
  */
 
 public class Cat {
+	private int id;
+	private String name;
+	private String color;
+
+	public Cat() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
 
 }
-
-
